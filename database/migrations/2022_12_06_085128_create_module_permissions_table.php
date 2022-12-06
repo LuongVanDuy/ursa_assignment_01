f23c3adb4cd566dedbe4198445a04f8aa74be194
Duy <nive54@gmail.com>
2022-12-06 17:09:04 +0700

    git add

---
+++ database/migrations/2022_12_06_085128_create_module_permissions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('module_permissions', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('module_id')->unsigned();
+            $table->integer('permission_id')->unsigned();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('module_permissions');
+    }
+};
